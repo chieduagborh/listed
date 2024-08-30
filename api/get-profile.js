728f76a86aeeffb851f5d5917ca81004a4ea54c3
Chiedu Agborh <chieduagborh@Chiedus-MacBook.local>
2024-08-30 12:24:22 +0100

    added get profile

---
+++ api/get-profile.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  // Handle CORS preflight requests
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, revision, Authorization');
+    res.status(200).end();
+    return;
+  }
+
+  // Handle GET request
+  if (req.method === 'GET') {
+    const klaviyoApiKey = process.env.KLAVIYO_API_KEY;
+    const email = req.query.email; // Assuming you pass the email as a query parameter
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, revision, Authorization');
+
+    try {
+       
+      const response = await axios.get(`https://a.klaviyo.com/api/profiles/?filter=equals(email,${email})`, {
+        headers: {
+          'Authorization': `Klaviyo-API-Key ${klaviyoApiKey}`,
+          'Content-Type': 'application/json',
+          'accept': 'application/json',
+          'revision': '2024-07-15'
+        }
+      });
+
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      if (error.response) {
+        console.error('Error details:', error.response ? error.response.data : error.message);
+        res.status(error.response.status).json(error.response.data);
+      } else {
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    }
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+};
